76ab729bbc59049fc109a300afe9d7f93ecaeabb
Xing Xing <mikespook@gmail.com>
2021-05-17 22:25:00 +1200

    fixed path

--- install.sh
+++ install.sh
@@ -1,15 +1,11 @@
 #/usr/bin/env bash
 config=$HOME/.config/nvim
-share=$HOME/.local/share/nvim
+share=$HOME/.local/nvim/share/nvim/site/autoload/
 
 
 [ -f $config ] && echo "$cnofig exists as a file" && exit
 [ -L $config ] && echo "$config exists as a symlink, point to `readlink -f $config`" && exit
 [ -e $config ] && echo "$config exists as a broken symlink, point ot `readlink -f $config`" && exit
 
-[ -f $share ] && echo "$share exists as a file" && exit
-[ -L $share ] && echo "$share exists as a symlink, point to `readlink -f $share`" && exit
-[ -e $share ] && echo "$share exists as a broken symlink, point ot `readlink -f $share`" && exit
-
 ln -s `pwd`/config $config
-ln -s `pwd`/share $share
+wget -O $share https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
