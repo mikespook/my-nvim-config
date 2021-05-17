ea9bb1cf69f0ed9a0da857977a46cfb333e2418f
Xing Xing <mikespook@gmail.com>
2021-05-17 20:30:45 +1200

    finished install

--- install.sh
+++ install.sh
@@ -4,8 +4,12 @@ share=$HOME/.local/share/nvim
 
 
 [ -f $config ] && echo "$cnofig exists as a file" && exit
-[ -L $config ] && echo "$config exists as a symlink, point to `readlink -f /home/mikespook/.config/nvim`" && exit
-[ -e $config ] && echo "$config exists as a broken symlink, point ot `readlink -f /home/mikespook/.config/nvim`" && exit
+[ -L $config ] && echo "$config exists as a symlink, point to `readlink -f $config`" && exit
+[ -e $config ] && echo "$config exists as a broken symlink, point ot `readlink -f $config`" && exit
 
-ln -s $config `pwd`/config
-ln -s $share `pwd`/share
+[ -f $share ] && echo "$share exists as a file" && exit
+[ -L $share ] && echo "$share exists as a symlink, point to `readlink -f $share`" && exit
+[ -e $share ] && echo "$share exists as a broken symlink, point ot `readlink -f $share`" && exit
+
+ln -s `pwd`/config $config
+ln -s `pwd`/share $share
